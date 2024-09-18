4afa3fae98edc1721d125dca0a666d308401e124
Carles Labrana <carles.labrana@idealiveconsulting.com>
2024-09-19 01:01:54 +0200

    DOCKER - MONGO DB

--- src/adapters/envs.adapter.ts
+++ src/adapters/envs.adapter.ts
@@ -8,4 +8,8 @@ export const envs = {
     MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
     MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
     MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+    MONGO_DB_URL : env.get('MONGO_DB_URL').required().asString(),
+    MONGO_DB_NAME: env.get('MONGO_DB_USER').required().asString(), 
+    MONGO_DB_USER: env.get('MONGO_DB_USER').required().asString(),
+    MONGO_DB_PASSWORD: env.get('MONGO_DB_USER').required().asString(),
 };
