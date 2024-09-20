990cf6c82dac287d49cbe9b10cf331787ddd8dc8
Carles Labrana <carles.labrana@idealiveconsulting.com>
2024-09-20 13:17:06 +0200

    POSTGRESQL & DOCKER

--- src/adapters/envs.adapter.ts
+++ src/adapters/envs.adapter.ts
@@ -5,11 +5,18 @@ export const envs = {
     PRODUCTION: env.get('PRODUCTION').required().asBool(),
     PORT: env.get('PORT').required().asPortNumber(),
     BASE_URL: env.get('BASE_URL').required().asString(),
+
     MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
     MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
     MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+
     MONGO_DB_URL : env.get('MONGO_DB_URL').required().asString(),
     MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(), 
     MONGO_DB_USER: env.get('MONGO_DB_USER').required().asString(),
     MONGO_DB_PASSWORD: env.get('MONGO_DB_PASSWORD').required().asString(),
+
+    POSTGRES_URL: env.get('POSTGRES_URL').required().asString(),
+    POSTGRES_DB: env.get('POSTGRES_DB').required().asString(),    
+    POSTGRES_USER: env.get('POSTGRES_USER').required().asString(),
+    POSTGRES_PASSWORD: env.get('POSTGRES_PASSWORD').required().asString(),
 };
